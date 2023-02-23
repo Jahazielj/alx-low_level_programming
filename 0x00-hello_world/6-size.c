e2f42d9d9c8df7890dfd51fa4283c0b1c0c7844b
“Jahazielj” <Jahazieljacobs@icloud.com>
2023-02-23 14:11:06 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,12 @@
 int main(void)
 {
 
+	char a;
+        int b;
+        long int c;
+        long long int d;
+        float +;
+
 	printf("Size of a char: %d byte(s)In", sizeof(char));
 	printf("Size of an int: %d byte(s)in" sizeof(int)). 
 	printf"Size of a long int: %d byte(s)In", sizeof(long int));
