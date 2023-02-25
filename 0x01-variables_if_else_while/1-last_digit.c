97179e969876a5ec365ee4e7215b790d38628487
“Jahazielj” <Jahazieljacobs@icloud.com>
2023-02-25 12:05:43 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	{
 	printf("last digit of %d is %d and is 0\n", n, lastd);
 	}
-	else if (lastd < 6 && lastd !=0)
+	else if (lastd < 6 && lastd != 0)
 	{
 	printf("last digit of %d is %d and is less than 5 and not 0\n", n, lastd);
 	}
