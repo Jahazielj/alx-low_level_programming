57b99ee4dca262721c1bdac1d31e17faef022053
“Jahazielj” <Jahazieljacobs@icloud.com>
2023-02-25 22:44:14 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphsbet - make the alphabet
+ * Return: void
+ */
+
+void print_alphsbet(void)
+
+{
+
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
