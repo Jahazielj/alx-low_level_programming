eed3c89d5d930aa8218b332a997f7ad831ec6925
“Jahazielj” <Jahazieljacobs@icloud.com>
2023-02-28 13:54:52 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (success)
@@ -9,23 +10,19 @@
 int main(void)
 
 {
-int d, p;
 
-for (d = '0'; d < '9'; d++)
-{
-for (p = d = 1; p <= '9'; p++)
-{
-if (p != d)
+int d;
+
+for (d = '0'; d <= '9'; d++)
 {
 putchar(d);
-putchar(p);
-if (d == '8' && p == '9')
-continue;
+if (d != '9')
+{
 putchar(',');
 putchar(' ');
 }
 }
-}
+
 putchar('\n');
 return (0);
 }
