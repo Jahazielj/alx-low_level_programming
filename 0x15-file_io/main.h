16d894cdb680a3b33d6f1df806ffb028f33e7b36
“Jahazielj” <Jahazieljacobs@icloud.com>
2023-04-14 20:27:04 -0700

    C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifdef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,4 +10,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* MAIN_H */
